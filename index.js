910cf0a3d151e94a30852705f88eb1f38138f68e
Malikic1 <azeezmalik19@gmail.com>
2024-05-15 02:16:07 +0100

    Adding Animation

--- index.js
+++ index.js
@@ -7,3 +7,73 @@ menuButton.addEventListener("click", function () {
   header.classList.toggle("height_header");
   header.style.borderRadius = "50px";
 });
+
+//Hide and show header as you scroll
+
+let lastScrollTop = 0;
+
+window.addEventListener(
+  "scroll",
+  function () {
+    let currentScroll =
+      window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScroll > lastScrollTop) {
+      // scrolling down
+      document.getElementById("header").style.top = "-100px"; // Hides the header
+    } else {
+      // scrolling up
+      document.getElementById("header").style.top = "1rem"; // Shows the header
+    }
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+  },
+  false
+);
+
+// rise up text animation
+document.addEventListener("DOMContentLoaded", function () {
+  const popups = document.querySelectorAll(".popup");
+
+  function checkPopup() {
+    popups.forEach((popup) => {
+      const triggerPosition = window.innerHeight - 20; // Adjust trigger position as per need
+      const popupPosition = popup.getBoundingClientRect().top;
+
+      if (popupPosition < triggerPosition) {
+        popup.classList.add("active");
+      } else {
+        popup.classList.remove("active");
+      }
+    });
+  }
+
+  window.addEventListener("scroll", checkPopup);
+  window.addEventListener("resize", checkPopup);
+
+  // Initial check in case some popups are already visible on page load
+  checkPopup();
+});
+
+//fadeIn animation
+document.addEventListener("DOMContentLoaded", function () {
+  const fadeIns = document.querySelectorAll(".fade-in");
+
+  function checkFadeIn() {
+    fadeIns.forEach((fadeIn) => {
+      const triggerPosition = window.innerHeight - 100; // Adjust trigger position as per need
+      const fadeInPosition = fadeIn.getBoundingClientRect().top;
+
+      if (fadeInPosition < triggerPosition) {
+        fadeIn.classList.add("active");
+      } else {
+        fadeIn.classList.remove("active");
+      }
+    });
+  }
+
+  window.addEventListener("scroll", checkFadeIn);
+  window.addEventListener("resize", checkFadeIn);
+
+  // Initial check in case some elements are already visible on page load
+  checkFadeIn();
+});
